f0b20e8aec8e7611e8a5b48d267c15a4f0b017b7
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-11-18 09:21:18 -0300

    Portafolio web v1.0

--- src/components/partials/nav/toggle.tsx
+++ src/components/partials/nav/toggle.tsx
@@ -39,7 +39,9 @@ export default function ComponentToggle(props: Props) {
                     <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
                         <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enterTo="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 translate-y-0 sm:scale-100" leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg dark:bg-primary bg-tertiary shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <ComponentIcon name="close" size={20} description_class="absolute right-2 top-2 text-red-500" />
+                                <div onClick={() => setOpen(false)} className="absolute right-2 top-2">
+                                    <ComponentIcon name="close" size={20} description_class="text-red-500" />
+                                </div>
                                 <div className='flex flex-col gap-5 py-9 px-4 w-[300px]'>
                                     <Dialog.Title as="h3" className="text-center font-semibold leading-6 text-secondary dark:text-tertiary border-b-[1px] dark:border-tertiary border-secondary pb-1 rounded-lg">
                                         {
