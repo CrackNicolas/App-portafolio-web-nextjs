e2cc573b30bfc153307ac185f486651830107207
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-11-20 18:38:33 -0300

    My portafolio v1.0

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -27,7 +27,7 @@ export default function ComponentLayout(props: Props) {
     }
 
     return (
-        <section id={id} className={` ${padding} ${isWidth ? 'px-[10px] sm:px-[22px]' : 'pl-[10px] sm:pl-[22px]'} lg:pl-[80px] flex flex-col gap-[20px] m-auto max-w-[1275px] pt-0`}>
+        <section id={id} className={`${padding} ${isWidth ? 'px-[10px] sm:px-[22px]' : 'pl-[10px] sm:pl-[22px]'} lg:pl-[80px] flex flex-col gap-[20px] m-auto max-w-[1275px] pt-0`}>
             <ComponentNameSection animate={animate} name={`${id}.title`} icon={id} />
             {children}
             {
